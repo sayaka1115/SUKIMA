50f7ab72287b005dc7c52352de5e5414cbaad366
sayaka1115 <sayaka.mizoguchi15@gmail.com>
2026-06-09 23:41:26 +0900

    （スケジュールをindexへ）

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :lectures
   resources :categories
 
-  root 'blogs#schedule'
+  root 'blogs#index'
   get 'schedule', to: 'blogs#schedule'
 
   get 'daily/:date',
